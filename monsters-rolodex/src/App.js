0a3ad021b7838a58e5f9234c7d6e75b20282a24f
Ihliana <Ihnatco@protonmail.com>
2024-03-26 13:12:40 +0100

    Update App.js
    Input field for searching and filtering

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { Component } from "react";
 
 class App extends Component {
+  //Initial state is set up
   constructor() {
     super();
     this.state = {
@@ -9,7 +10,9 @@ class App extends Component {
     };
   }
 
+  //lifecycle method
   componentDidMount() {
+    //fetching user data and update state
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((users) =>
@@ -19,9 +22,29 @@ class App extends Component {
       );
   }
 
+  //generating the initial UI based on the initial state
   render() {
     return (
       <div className="App">
+        {/*Input field for searching monsters*/}
+        <input
+          type="search"
+          className="search"
+          placeholder="Search monsters"
+          onChange={(event) => {
+            const searchString = event.target.value.toLocaleLowerCase();
+
+            const filteredMonsters = this.state.monsters.filter((monster) => {
+              return monster.name.toLocaleLowerCase().includes(searchString);
+            });
+
+            this.setState(() => {
+              return { monsters: filteredMonsters };
+            });
+          }}
+        />
+
+        {/*Displaying monsters*/}
         {this.state.monsters.map((monster) => {
           return (
             <div key={monster.id}>
