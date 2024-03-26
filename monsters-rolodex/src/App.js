2facbc2530624535d1cdde5bdea6834f70e42b59
Ihliana <Ihnatco@protonmail.com>
2024-03-26 13:24:25 +0100

    Update App.js
    separating orignal data from filtered data

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
     super();
     this.state = {
       monsters: [],
+      searchField: "",
     };
   }
 
@@ -24,6 +25,10 @@ class App extends Component {
 
   //generating the initial UI based on the initial state
   render() {
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    });
+
     return (
       <div className="App">
         {/*Input field for searching monsters*/}
@@ -32,20 +37,15 @@ class App extends Component {
           className="search"
           placeholder="Search monsters"
           onChange={(event) => {
-            const searchString = event.target.value.toLocaleLowerCase();
-
-            const filteredMonsters = this.state.monsters.filter((monster) => {
-              return monster.name.toLocaleLowerCase().includes(searchString);
-            });
-
+            const searchField = event.target.value.toLocaleLowerCase();
             this.setState(() => {
-              return { monsters: filteredMonsters };
+              return { searchField };
             });
           }}
         />
 
         {/*Displaying monsters*/}
-        {this.state.monsters.map((monster) => {
+        {filteredMonsters.map((monster) => {
           return (
             <div key={monster.id}>
               <h1>{monster.name}</h1>
