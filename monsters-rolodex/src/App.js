2abe2f5beea8b110ab582a3eed1302869ac8e066
Ihliana <Ihnatco@protonmail.com>
2024-03-26 12:50:00 +0100

    Update Appj.js
    initial rendering 'state', 'setState', 'componentDidMount' lifecycle

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,7 +1,37 @@
 import "./App.css";
+import { Component } from "react";
 
-function App() {
-  return <div className="App"></div>;
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(() => {
+          return { monsters: users };
+        })
+      );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.monsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
